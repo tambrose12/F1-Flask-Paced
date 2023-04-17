56c8da64640058136529a9d4c40834245dd96ba6
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-04-17 12:04:38 -0400

    created tables in db and added get routes for races/drivers

--- server/app.py
+++ server/app.py
@@ -3,14 +3,62 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import db, Driver, Race, DriverRace
 
 # Views go here!
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return "Welcome to F1 Flask Paced"
+    
+api.add_resource(Home, '/')
+
+class Drivers(Resource):
+    def get(self):
+        d_list = []
+        for d in Driver.query.all():
+            d_dict = {
+                'id': d.id,
+                'name': d.name,
+                'car_number': d.car_number,
+                'team': d.team
+            }
+            d_list.append(d_dict)
+        return make_response(d_list, 200)
+    
+    def post(self):
+        pass
+    
+api.add_resource(Drivers, '/drivers')
+
+class Races(Resource):
+    def get(self):
+        r_list = []
+        for r in Race.query.all():
+            r_dict = {
+                'id': r.id,
+                'location': r.location,
+                'fastest_time': r.fastest_time
+            }
+            r_list.append(r_dict)
+        return make_response(r_list, 200)
+    
+api.add_resource(Races, '/races')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-from config import db
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-# Models go here!
+db = SQLAlchemy(metadata=metadata)
+
+class Driver(db.Model, SerializerMixin):
+    __tablename__ = 'drivers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    car_number = db.Column(db.Integer)
+    team = db.Column(db.String)
+
+    driver_races = db.relationship('DriverRace', backref = 'driver', cascade = 'all, delete-orphan')
+    races = association_proxy('driver_races','race')
+
+class Race(db.Model, SerializerMixin):
+    __tablename__ = 'races'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String)
+    fastest_time = db.Column(db.Float)
+
+    driver_races = db.relationship('DriverRace', backref = 'race', cascade = 'all, delete-orphan')
+    drivers = association_proxy('driver_races','driver')
+
+class DriverRace(db.Model, SerializerMixin):
+    __tablename__ = 'driver_races'
+
+    id = db.Column(db.Integer, primary_key=True)
+    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'))
+    race_id = db.Column(db.Integer, db.ForeignKey('races.id'))
+    time = db.Column(db.Float)
