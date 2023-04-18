10b7444f0e23b86a326f2ecd03918fad78a5e26e
Taylor Ambrose <taykesby@gmail.com>
2023-04-18 10:16:43 -0400

    added post, patch, and delete to driver

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
@@ -22,11 +23,14 @@ migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
 
+
 class Home(Resource):
     def get(self):
         return "Welcome to F1 Flask Paced"
-    
+
+
 api.add_resource(Home, '/')
+
 
 class Drivers(Resource):
     def get(self):
@@ -40,11 +44,25 @@ class Drivers(Resource):
             }
             d_list.append(d_dict)
         return make_response(d_list, 200)
-    
+
     def post(self):
-        pass
-    
+        data = request.get_json()
+        new_driver = Driver(
+            name=data['name'], car_number=data['car_number'], team=data['team'])
+
+        try:
+            db.session.add(new_driver)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({'error': 'All inputs need valid data'})
+
+        driver_dict = new_driver.to_dict()
+        return make_response(driver_dict, 201)
+
+
 api.add_resource(Drivers, '/drivers')
+
 
 class DriverByID(Resource):
     def get(self, id):
@@ -52,8 +70,42 @@ class DriverByID(Resource):
         if d == None:
             return make_response({'error': 'Driver not found'}, 404)
         return make_response(d.to_dict(), 200)
-    
+
+    def patch(self, id):
+        data = request.get_json()
+        driver = Driver.query.filter_by(id=id).first()
+        if driver == None:
+            return make_response({'error': 'Driver not found'}, 404)
+
+        for attr in data:
+            setattr(driver, attr, data[attr])
+
+        try:
+            db.session.add(driver)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return make_response({'error': 'validation errors'}, 422)
+
+        response_dict = driver.to_dict()
+
+        response = make_response(response_dict, 200)
+
+        return response
+
+    def delete(seld, id):
+        driver = Driver.query.filter_by(id=id).first()
+        if driver == None:
+            return make_response({'error': 'Driver not found'}, 404)
+
+        db.session.delete(driver)
+        db.session.commit()
+
+        return make_response('Apartment Deleted', 201)
+
+
 api.add_resource(DriverByID, '/drivers/<int:id>')
+
 
 class Races(Resource):
     def get(self):
@@ -66,8 +118,10 @@ class Races(Resource):
             }
             r_list.append(r_dict)
         return make_response(r_list, 200)
-    
+
+
 api.add_resource(Races, '/races')
+
 
 class RaceByID(Resource):
     def get(self, id):
@@ -75,8 +129,10 @@ class RaceByID(Resource):
         if r == None:
             return make_response({'error': 'Race not found'}, 404)
         return make_response(r.to_dict(), 200)
-    
+
+
 api.add_resource(RaceByID, '/races/<int:id>')
+
 
 class DriverRaces(Resource):
     def get(self):
@@ -90,7 +146,8 @@ class DriverRaces(Resource):
             }
             dr_list.append(dr_dict)
         return make_response(dr_list, 200)
-    
+
+
 api.add_resource(DriverRaces, '/driver_races')
 
 if __name__ == '__main__':
